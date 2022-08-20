da4b27c52442e016a93ef207835a78e944c664d8
Yevhenii Lymarenko <jekilllimarenko@gmail.com>
2022-08-20 14:43:34 +0300

    fix all mistakes + add control form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -65,3 +65,36 @@ export class App extends Component {
     );
   }
 }
+
+
+
+// Контролируемая форма 
+
+// import { GlobalStyle } from "./GlobalStyle";
+// import { Box } from "./Box";
+
+// import { Component } from 'react';
+// import { Form } from './Form/Form';
+
+// export class App extends Component {
+//   state = {
+//     contacts: [],
+//     name: '',
+//     number: '',
+//   };
+
+
+// handlerFormSubmit = data => {
+//   console.log(data);
+// }
+
+
+//   render() {
+//     return (
+//       <>
+//         <h1>Phonebook</h1>
+//         <Form onSubmit={this.handlerFormSubmit} />
+//       </>
+//     );
+//   }
+// }
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -3,7 +3,7 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 import PropTypes from 'prop-types';
 
-
+// Создание формы с библиотекой Formik
 
 const schema = yup.object().shape({
   name: yup.string().required(),
@@ -57,3 +57,76 @@ export const ContactForm = ({ onSubmit }) => {
 ContactForm.propTypes ={
   onSubmit: PropTypes.func.isRequired,
 }
+
+
+// Контролируемая форма
+
+
+
+// import { Component } from 'react';
+// import { nanoid } from "nanoid"
+
+// export class Form extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   nameInputId = nanoid();
+//   numberInputId = nanoid();
+
+//   handleChange = event => {
+//     const { name, value } = event.currentTarget;
+
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     this.props.onSubmit(this.state);
+//     this.reset();
+
+//   };
+
+// reset = () => {
+//     this.setState({
+//         name: '',
+//         number: '',
+//     })
+// }
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label htmlFor={this.nameInputId}>
+//             Name
+//         <input
+//           type="text"
+//           name="name"
+//           value={this.state.name}
+//           onChange={this.handleChange}
+//           id={this.nameInputId}
+//           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//           required
+//         />
+// </label>
+//         <label htmlFor={this.numberInputId}>
+//             Number
+//         <input
+//           type="tel"
+//           name="number"
+//           value={this.state.number}
+//           onChange={this.handleChange}
+//           id={this.numberInputId}
+//           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//           required
+//         />
+
+//         <button type="submit">Add contact</button>
+//         </label>
+//       </form>
+//     );
+//   }
+// }
